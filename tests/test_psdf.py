cb29f28f8aef083292a380e5383fb9e2552541c3
Saeran <saeranv@gmail.com>
2023-03-15 18:01:47 -0400

    Sync.

--- tests/test_psdf.py
+++ tests/test_psdf.py
@@ -4,8 +4,8 @@ import numpy as np
 from thiru import psdf
 
 
-def _tocol(pt):
-    return np.array(pt)[:, np.newaxis]
+def _vec(*args):
+    return np.array(args)[:, np.newaxis]
 
 def test_point_sdf():
     """Test SDF of point.
@@ -13,14 +13,25 @@ def test_point_sdf():
     Given point (0, 0), SDF should evaluate to 1 at (0, 0), and
     0 elsewhere.
     """
-    pt = np.array([[0], [0]])
-    pt_sdf = psdf.point_sdf(pt)
+    # Base case at origin
+    pt_sdf = psdf.point_sdf(_vec(0, 0))
 
-    eval_pt = _tocol([0, 0])
-    sdf = pt_sdf(eval_pt)
-    assert np.abs(sdf - 1.0) < 1e-6, sdf
-    eval_pt = _tocol([0.5, 0])
+    eval_pt = _vec(0, 0)
     sdf = pt_sdf(eval_pt)
     assert np.abs(sdf - 0.0) < 1e-6, sdf
+    eval_pt = _vec(1, 0)
+    sdf = pt_sdf(eval_pt)
+    assert np.abs(sdf - 1.0) < 1e-6, sdf
+
+    # Off origin
+    pt_sdf = psdf.point_sdf(_vec(-1.0, -1.0))
+
+    eval_pt = _vec(0, 0)
+    sdf = pt_sdf(eval_pt)
+    _hypot = np.sqrt(2)
+    assert np.abs(sdf - _hypot) < 1e-6, sdf
+    eval_pt = _vec(-1, 0)
+    sdf = pt_sdf(eval_pt)
+    assert np.abs(sdf - 1.0) < 1e-6, sdf
 
 
