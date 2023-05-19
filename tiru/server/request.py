97374827051d69e13f981180fdd514304d7be054
Saeran <saeranv@gmail.com>
2023-05-19 12:00:01 -0400

    Automatic backup.

--- tiru/server/request.py
+++ tiru/server/request.py
@@ -2,19 +2,11 @@ from __future__ import annotations
 import sys
 import typing as typ
 import requests
-from io import BytesIO, StringIO
+# from io import BytesIO, StringIO
 from argparse import ArgumentParser
+from select import select
 
-
-def timed_stdin() -> str:
-    """Read stdin """
-    # Check if piped data exists
-    rlist, wlist, xlist = [sys.stdin], [], []
-    timeout = 0.5
-    rlist = select(rlist, wlist, xlist, timeout)[0]
-    # return sys.stdin.read() if rlist else []
-    return read_binary() if rlist else []
-
+URL = 'http://127.0.0.1:8100/'
 
 # def read_binary() -> bytes:
     # """Read binary stream from stdin."""
@@ -33,31 +25,40 @@ def timed_stdin() -> str:
             # byte_file.write(chunk)
 #
     # return byte_file
-#
 
-def post_binary_image(byte_file:bytes) -> None:
-    url = 'http://127.0.0.1:8100/'
+def read_timed_stdin() -> str|list[typ.Any]:
+    """Read stdin """
+    # Check if piped data exists
+    rlist, wlist, xlist = [sys.stdin], [], []
+    timeout = 0.5
+    rlist = select(rlist, wlist, xlist, timeout)[0]
+    return sys.stdin.read() if rlist else []
+    # return read_binary() if rlist else []
+
+
+def post_binary_image(byte_file:bytes, url:str) -> None:
     img_uri = byte_file
     data = {'message': img_uri}
     response = requests.post(url, json=data)
-    #print(response.text)
+    print(response.text)
 
 
 if __name__ == "__main__":
 
+    # TODO: replace with invoke
     parser = ArgumentParser(
         prog='tiru', description='??')
     parser.add_argument('-img', type=str)
     parser.add_argument('-str', type=str)
 
     args, stdin_arr = parser.parse_known_args()
-
     if not stdin_arr:
         stdin_arr = read_timed_stdin()
-    args = parser.parse_args()
+
     print(args.img)
 
-    if args.img:
-        byte_file = read_binary()
-        post_binary_image(byte_file)
+    if args.img and len(stdin_arr) > 0:
+        # byte_file = read_binary()
+        byte_file = stdin_arr[0]
+        post_binary_image(byte_file, url=URL)
 
