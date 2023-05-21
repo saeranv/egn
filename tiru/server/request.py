9bb1e4b8740ef8779676092060645ec31cc2e391
Saeran <saeranv@gmail.com>
2023-05-21 15:30:01 -0400

    Automatic backup.

--- tiru/server/request.py
+++ tiru/server/request.py
@@ -22,29 +22,15 @@ if __name__ == "__main__":
         prog='tiru', description='??')
     parser.add_argument(
         '-img', '--img_file', type=FileType('rb'),
-        help=('# to stream file as <stdin> use "-" as arg:\n'
-              'cat img.jpg | request.py -img -\n'
-              '# to add filename: \n'
+        help=('# Accepts img filepath: \n'
               'request.py -img ./img.jpg')
-    )
-    # args, stdin_arr = parser.parse_known_args()
+              '# To stream file as <stdin> use "-" as arg:\n'
+              'cat img.jpg | request.py -img -\n'))
     args =  parser.parse_args()
 
-    # if stdin_arr:
-        # # TODO: fix. Doesn't work because input is text which can't cast
-        # # TODO: just make optional narg in -img arg and specify bytes?
-        # assert False, "Only piped stdin works."
-        # # _stdin_arr = bytearray()
-        # # [_stdin_arr.append(bytes(x, 'utf-8')) for x in stdin_arr]
-        # # stdin_arr = _stdin_arr
-    # # else:
-        # stdin_arr = read_timed_stdin('b')
-        # stdin_arr = [base64.b64encode(stdin_arr).decode()]
-
-    # print('img: ', args.img, 'txt: ', args.txt)
     if args.img_file:
-        # args.img is file object io.BufferedReader, convert to base64 str
-        # to send through json
+        # args.img is file object io.BufferedReader
+        # convert to base64 str to send through json
         byte_data = args.img_file.read()  # bytes
         byte_b64_str = base64.b64encode(byte_data).decode('utf-8') # base64 str
-        post_binary_image(bytes_b64_str, url=URL)
+        post_byte_image(byte_b64_str, url=URL)
