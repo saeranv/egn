a2af1ee67f46c2bf917883ddb80ea253d5249439
Saeran <saeranv@gmail.com>
2023-05-28 15:30:01 -0400

    Automatic backup.

---
+++ tiru/analytical/holford.py
@@ -0,0 +1,28 @@
+# Holford 2007
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Models naturally ventilated space with internal thermal mass in response to
+harmonic variation in T_ext. T_int is function of heat balance between
+thermal mass (C m = kg J/kgK), and environment
+
+Should reduce hot
+temperature significantly when T_ext >= T_int, if building is thermally
+massive.
+
+If internal gains are small, diurnal variations are high.
+
+
+
+
+Assumes:
+- internal gains small
+- internal walls are adiabatic
+- no solar gains
+
+
+
+"""
+
