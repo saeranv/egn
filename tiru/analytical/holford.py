16dfce6674e2a0161736bca099894d99fe18ff3b
Saeran <saeranv@gmail.com>
2023-05-29 12:30:01 -0400

    Automatic backup.

--- tiru/analytical/holford.py
+++ tiru/analytical/holford.py
@@ -9,17 +9,15 @@ harmonic variation in T_ext. T_int is function of heat balance between
 thermal mass (C m = kg J/kgK), and environment via natvent (vol_nv/s = m3/s).
 
 Dynamic:
-- When T_ext >= T_int, Cm inversely correlates to T_int (<< Cm leads to >> T_int).
+- When T_ext >= T_int, Cm inversely correlates to T_int (<< Cm leads to >>
+  T_int).
 - When << q_int, >> diurnal swing b/c T_int varies from T_ext widely.
-- When >> q_int, << diurnal swings b/c loosely follows T_ext. B/c << q_int drives << vol_nv/s. While
-  T_int is higher, expect more rapid flushing due to higher temperature delta.
+- When >> q_int, << diurnal swings b/c T_int follows T_ext, as q_int drives
+  vol_nv/s.
 
 
-
-
-
-Assumes:
-- internal gains small
+Assumes as constant:
+- >> internal gains
 - internal walls are adiabatic
 - no solar gains
 
