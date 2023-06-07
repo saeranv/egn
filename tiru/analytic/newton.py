9b660271ee70e7052dca787eb766ab323aa6239b
Saeran <saeranv@gmail.com>
2023-06-07 15:00:01 -0400

    Automatic backup.

--- tiru/analytic/newton.py
+++ tiru/analytic/newton.py
@@ -10,6 +10,24 @@ def newton(
     ) -> np.ndarray:
     """Predict array of temperatures for node given parameters.
 
+    1. Derivation of T(t) = (T0 - T_ext) exp(a t) + T_ext
+    Newton's law states that temperature rate of lumped node decreases in
+    proportion to difference of ambient temperature.
+        dT/dt = K T - T_ext; where K is some constant of proportionality
+        y(t) = T(t) - T_ext
+        ; Simplify spatial diff as y
+        dy  = dT = (T_1 - T_ext) - (T_0 - T_ext)
+        dy/dt = K y.
+        ; As exponential fn
+        y(t) = C exp(a t)
+        dy/dt = a C exp(a t); where C = y(0) = C exp(a 0)
+        ; Substitute back original variables:
+        T(t) - T_ext = C exp(a t), b/c y(t) = T(t) - T_ext
+        T(t) = (T(0) - T_ext) exp(a t) + T_ext
+
+    2. Derivation of a, diffusivity coefficient.
+
+
     Args:
         # Diffusivity (alpha) params: hA/pCV
         h_c: Node surface convective coefficient [W/m2-K]
