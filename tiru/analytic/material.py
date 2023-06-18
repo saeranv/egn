cf8dc06c9370571fb258bcbfa2e4c99b62fff764
Saeran <saeranv@gmail.com>
2023-06-18 02:00:01 -0400

    Automatic backup.

--- tiru/analytic/material.py
+++ tiru/analytic/material.py
@@ -1,21 +1,47 @@
 import numpy as np
 
 
-def diffusivity_coef():
-    """."""
-    pass
+def diffusivity_coef(k:float, rho:float, C_p:float) -> float:
+    """Diffusivity coefficient alpha = k / rho-C [m2/s].
 
-
-def fourier_coef(alpha, char_len, dt):
-    """Fourier coefficient (alpha dt / L2).
+    Units k / rho-C
+        = W/m-K / kg/m3-J/kg-K
+        = m2/s
 
     Args:
-        alpha: k / rho C  # diffusivity [units]
-        Fo = k dt / L2 rho C
+        k: conductivity [W/m-K]
+        rho: density [kg/m3]
+        C_p: specific heat capacity at constant pressure [J/kg-K]
+
+    Returns diffusivity coefficient [m2/s].
+    """
+    assert k >= 1e-10    # not adiabatic
+    assert rho >= 1e-10  # must have density
+    assert C_p >= 1e-10  # must have heat capacity
+
+    return k / (rho * C_p)
+
+
+def fourier_coef(alpha:float, char_len:float, dt:float) -> float:
+    """Dimensionless fourier coefficient (alpha-dt / L2) [-].
+
+    Units dt-alpha / L2
+        = s-alpha / m2
+        = s-(m2/s) / L2,    alpha units is m2/s
+        = - / -
+
+    Args:
+        alpha: diffusivity coefficient [m2/s]
+        char_len: characteristic length [m]
+        dt: time step [s]
 
     Returns Fourier coefficient.
     """
-    pass
+    assert alpha >= 1e-10  # k > 0 so (k / rho-C) > 0
+    assert char_len >= 1e-10  # must have thickness
+    assert dt >= 1e-10  # must have timestep
+
+    return (alpha * dt) / (char_len * char_len)
 
 
 def biot_coef():
