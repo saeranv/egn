aa50065b3842b9c8e9eb526748c8b2566504effb
Saeran <saeranv@gmail.com>
2023-06-19 22:30:01 -0400

    Automatic backup.

--- tiru/analytic/test_analytic.py
+++ tiru/analytic/test_analytic.py
@@ -3,7 +3,7 @@
 import pytest
 import numpy as np
 import material as mat
-
+import heat as heat
 
 def test_diffusivity():
     """Test derivation of thermal diffusivity."""
@@ -47,37 +47,48 @@ def test_fourier_coef():
     assert np.abs(fo - fo_) < 1e-10
 
 
+def test_lumped_node():
+    """Test mat law of cooling."""
 
-# def test_mat():
-    # """Test mat law of cooling."""
-#
-    # # Diffusivity (alpha) params: hA/pCV
-    # h_c = 1 # W/m2-K
-    # A = 1 # m2
-    # p = 1 # kg/m3
-    # Cp = 1 # specific heat J/kg-K
-    # Vol = 1 # m3
-#
-    # # Initial temperatures
-    # T_int_0 = 100 # C
-    # T_ext = 23 # C
-#
-    # # Time params
-    # dt = 0.1 # s
-    # Nt = 3600 # s
-#
-    # # Solve
-    # T_int = mat.mat(
-        # h_c, A, p, Cp, Vol,
-        # T_int_0, T_ext,
-        # dt, Nt
-    # )
-#
-    # assert isinstance(T_int, np.ndarray), type(T_int)
-    # assert T_int.shape == (Nt/dt,), T_int.shape
-    # assert T_int_0 > T_ext
-    # assert T_int[0] == T_int_0
-    # assert T_int[1] < T_int_0
-    # assert abs(np.min(T_int) - T_int[-1]) <= 1e-10
-#
-#
+    # Diffusivity (alpha) params: hA/pCV
+    h_c = 1 # W/m2-K
+    area = 1 # m2
+    rho = 1 # kg/m3
+    C_p = 1 # specific heat J/kg-K
+    vol = 1 # m3
+
+    # Initial temperatures
+    T0 = 100 # C
+    T_ext = 23 # C
+
+    # Time params
+    t = 30 # s
+
+    eps = 1e-10
+    assert T0 > -273.15; assert T_ext > -273.15
+    assert h_c >= eps; assert area >= eps
+    assert C_p >= eps; assert vol >= eps; assert rho >= eps
+    assert t >= eps
+
+    # Derive dimensionless params
+    char_len = vol / area  # [m]
+    bi = mat.biot_coef(h_c, char_len, k)
+    alpha = mat.diffusivity_coef(k, rho, C_p)
+    fo = mat.fourier_coef(alpha, char_len, dt)
+
+    # Convert to K, and then dimensionless theta
+    T0 += 273.15; T_ext += 273.15 # K
+    delta_T = np.abs(T0 - T_ext)
+
+    # theta(t) = T(t) / delta_T: dimensionless temp
+    # T = theta * delta_T
+    theta = heat.lumped_node(bi, fo)
+    T_int = theta * delta_T
+
+    assert isinstance(T_int, np.ndarray), type(T_int)
+    assert T_int.shape == t, T_int.shape
+    assert T0 > T_ext
+    assert T_int[0] == T0
+    assert T_int[1] < T0
+    assert abs(np.min(T_int) - T_int[-1]) <= 1e-10
+
