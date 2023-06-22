93c9c1686ce23bee203593689fb8927b72637af3
Saeran <saeranv@gmail.com>
2023-06-22 18:00:01 -0400

    Automatic backup.

--- tiru/scratch.py
+++ tiru/scratch.py
@@ -1,1 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import viz
+
+# "rY
+# :!tmux send-keys -t x.2 "cht vim" ENTER
+print(viz)
+
+
+
 
--- tiru/sscratch.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import viz
-
-
-
-print(viz)
-
-
