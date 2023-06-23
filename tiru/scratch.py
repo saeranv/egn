241d9dc8b17b40f8b1ed8308291280c21f52cf9c
Saeran <saeranv@gmail.com>
2023-06-22 22:30:01 -0400

    Automatic backup.

--- tiru/scratch.py
+++ tiru/scratch.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def fact(n1,n0):
-    return np.prod(np.arange(n0,n1+1, dtype=np.float64))
+    return np.prod(np.arange(n0, n1+1, dtype=np.float64))
 
 
-p = fact(365,365-23) / (365**23)
+p = fact(365,365-23) / (365 ** 23)
 
 print(1 - p)
